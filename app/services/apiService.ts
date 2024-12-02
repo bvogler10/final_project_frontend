5ae37e79fa311cf5d92f3aa0b66d88fd6c2a3db4
bvogler10 <brinjavogler@gmail.com>
2024-12-01 22:09:30 -0500

    created api service

--- app/services/apiService.ts
+++ app/services/apiService.ts
@@ -1,6 +1,47 @@
 const apiService = {
     get: async function (url: string): Promise<any> {
         console.log('get', url)
+
+        return new Promise((resolve, reject) => {
+            fetch(`${process.env.NEXT_PUBLIC_API_URL}${url}`, {
+                method: 'GET',
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                }
+            })
+            .then(response => response.json())
+            .then((json) => {
+                console.log('Response', json);
+
+                resolve(json);
+            }) 
+            .catch((error) => {
+                reject(error);
+            })
+        })
+    },
+    post: async function(url: string, data: any): Promise<any> {
+        console.log('post', url, data);
+        return new Promise((resolve, reject) => {
+            fetch(`${process.env.NEXT_PUBLIC_API_URL}${url}`, {
+                method: 'POST',
+                body: data,
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                }
+            })
+            .then(response => response.json())
+            .then((json) => {
+                console.log('Response', json);
+
+                resolve(json);
+            }) 
+            .catch((error) => {
+                reject(error);
+            })
+        })
     }
 }
 
--- components/pages/explore/PostList.tsx
+++ components/pages/explore/PostList.tsx
@@ -1,7 +1,7 @@
 // Define the PostList component
 
 import { FC, useEffect, useState } from "react";
-
+import apiService from "@/app/services/apiService";
 import { PostListItem } from "./PostListItem";
 import { Post } from "@/types/Post";
 
@@ -9,34 +9,23 @@ interface PostListProps {
     endpoint: string;
   }
   
-  export const PostList: FC<PostListProps> = ({ endpoint }) => {
+export const PostList: FC<PostListProps> = ({ endpoint }) => {
     const [posts, setPosts] = useState<Post[]>([]);
     const getPosts = async () => {
-        const url = 'http://localhost:8000' + endpoint;
+        const tmpPosts = await apiService.get(endpoint);
 
-        await fetch(url, {
-            method: 'GET',
-        })
-        .then(response => response.json())
-        .then((json) => {
-            console.log('json', json);
-            const sortedPosts = json.data.sort((a: Post, b: Post) => {
-                if (new Date(b.created_at) > new Date(a.created_at)) return 1;
-                if (new Date(b.created_at) < new Date(a.created_at)) return -1;
-                return 0;
-            });
-            setPosts(sortedPosts);
-        })
-
-        .catch((error) => {
-            console.log('error', error);
-        })
+        const sortedPosts = tmpPosts.data.sort((a: Post, b: Post) => {
+            if (new Date(b.created_at) > new Date(a.created_at)) return 1;
+            if (new Date(b.created_at) < new Date(a.created_at)) return -1;
+            return 0;
+        });
+        setPosts(sortedPosts);
     };
 
     useEffect(() => {
         getPosts();
-      }, [endpoint]);
-  
+    }, [endpoint]);
+
     return (
         <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-3">
             {posts.map((post) => (
@@ -46,6 +35,6 @@ interface PostListProps {
                 isFollowing={false}
                 />
             ))}
-      </div>
+        </div>
     );
-  };
+};
