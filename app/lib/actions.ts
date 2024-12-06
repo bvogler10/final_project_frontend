6042bc0bedbab23004ceb6f80a5b9e5a0622ec24
bvogler10 <brinjavogler@gmail.com>
2024-12-05 23:17:24 -0500

    refresh token

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -36,7 +36,66 @@ export async function getUserId() {
     return userId ? userId : null
 }
 
+// Function to get the access token from the session cookie
 export async function getAccessToken() {
-    const token = (await cookies()).get('session_access_token')?.value
+    const cookieStore = await cookies();
+    let accessToken = cookieStore.get("session_access_token")?.value;
+  
+    if (!accessToken) {
+      accessToken = await handleRefresh();
+    }
+    return accessToken;
+  }
+
+export async function getRefreshToken() {
+    const token = (await cookies()).get('session_refresh_token')?.value
     return token ? token : null
 }
+
+// Function to refresh the access token when it expires
+export async function handleRefresh() {
+    const refreshToken = await getRefreshToken();
+    const cookieStore = await cookies();
+  
+    try {
+      const response = await fetch(
+        `${process.env.NEXT_PUBLIC_API_URL}/auth/token/refresh/`,
+        {
+          method: "POST",
+          body: JSON.stringify({ refresh: refreshToken }),
+          headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+          },
+        }
+      );
+  
+      if (!response.ok) {
+        // Handle HTTP errors
+        console.error("Refresh token error:", await response.text());
+        resetAuthCookies();
+        return null;
+      }
+  
+      const json = await response.json();
+  
+      if (json.access) {
+        cookieStore.set("session_access_token", json.access, {
+          httpOnly: true,
+          secure: process.env.NODE_ENV === "production",
+          maxAge: 60 * 60, // 60 minutes
+          path: "/",
+        });
+  
+        return json.access;
+      } else {
+        resetAuthCookies();
+        return null;
+      }
+    } catch (error) {
+      console.error("Error refreshing token:", error);
+      resetAuthCookies();
+      return null;
+    }
+  }
+  
