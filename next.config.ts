fba4cf11fab9af2dfd32e1ed3d4a9156416df474
bvogler10 <brinjavogler@gmail.com>
2024-12-09 12:55:49 -0500

    added backend image origins

--- next.config.ts
+++ next.config.ts
@@ -5,13 +5,20 @@ const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: 'http',
-        hostname: 'localhost',
-        port:'8000',
-        pathname: '/**'
-      }
-    ]
-  }
+        protocol: "http",
+        hostname: "localhost",
+        port: "8000",
+        pathname: "/**",
+      },
+      // Production environment
+      {
+        protocol: "https",
+        hostname: "closeknit-backend-74fd3b290357.herokuapp.com",
+        port: "",
+        pathname: "/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
