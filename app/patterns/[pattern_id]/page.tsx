30920374ef2f5823e53c311ebcc2d3633e29d659
bvogler10 <brinjavogler@gmail.com>
2024-12-10 14:55:36 -0500

    removed import

--- app/patterns/[pattern_id]/page.tsx
+++ app/patterns/[pattern_id]/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { useEffect, useState } from "react";
-import Image from "next/image";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Badge } from "@/components/ui/badge";
 import { useParams } from "next/navigation";
